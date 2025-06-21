2ceb86f3204ff8082f3e21877525689528b453bf
devpilotlab <developer@devpilotlab.dev>
2025-06-21 18:11:42 +0900

    proxy設定

--- react-tree-app1/web-frontend/vite.config.ts
+++ react-tree-app1/web-frontend/vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // 開発サーバーの設定
+    port: 3001, // フロントエンドのポート番号
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', // バックエンドAPIのURL
+        changeOrigin: true,
+      }
+    }
+  },
 })
